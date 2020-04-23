cf2fa1ed1929fec1291d9b7aaf7c77143a90b5a7
Florian Lüke <f.lueke@mesytec.com>
2020-04-23 16:32:42 +0200

    move git version constants into the mesytec::mvlc namespace

--- src/mesytec-mvlc/git_version.h
+++ src/mesytec-mvlc/git_version.h
@@ -1,9 +1,17 @@
 #ifndef __MESYTEC_MVLC_GIT_VERSION_H__
 #define __MESYTEC_MVLC_GIT_VERSION_H__
 
+namespace mesytec
+{
+namespace mvlc
+{
+
 extern const char GIT_SHA1[];
 extern const char GIT_VERSION[];
 extern const char GIT_VERSION_SHORT[];
 extern const char GIT_VERSION_TAG[];
+
+} // end namespace mvlc
+} // end namespace mesytec
 
 #endif /* __MESYTEC_MVLC_GIT_VERSION_H__ */
