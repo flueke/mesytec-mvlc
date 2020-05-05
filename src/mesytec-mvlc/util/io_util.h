95b0b02e94631990977fc1d8ffbfd288f615bc9f
Florian Lüke <f.lueke@mesytec.com>
2020-05-05 11:39:29 +0200

    add log_buffer variant printing only the start and end of the input buffer

--- src/mesytec-mvlc/util/io_util.h
+++ src/mesytec-mvlc/util/io_util.h
@@ -26,6 +26,31 @@ Out &log_buffer(Out &out, const View &buffer, const std::string &header = {})
     return out;
 }
 
+template<typename Out, typename View>
+Out &log_buffer(Out &out, const View &buffer, const std::string &header,
+                size_t numStartWords, size_t numEndWords)
+{
+    numStartWords = std::min(numStartWords, buffer.size());
+    numEndWords = std::min(numEndWords, buffer.size());
+
+
+    out << "begin buffer '" << header << "' (size=" << buffer.size() << ")" << std::endl;
+
+    out << numStartWords << " first words:" << std::endl;
+
+    for (size_t i=0; i<numStartWords; i++)
+        out << fmt::format("  0x{:008X}", buffer[i]) << std::endl;
+
+    out << numEndWords << " last words:" << std::endl;
+
+    for (size_t i=buffer.size() - (numEndWords + 1); i < buffer.size(); i++)
+        out << fmt::format("  0x{:008X}", buffer[i]) << std::endl;
+
+    out << "end buffer " << header << "' (size=" << buffer.size() << ")" << std::endl;
+
+    return out;
+}
+
 } // end namespace util
 } // end namespace mvlc
 } // end namespace mesytec
