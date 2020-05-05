ce248697c003b72bc71f4eeac770b24932103090
Florian Lüke <f.lueke@mesytec.com>
2020-05-05 18:21:37 +0200

    add a listfile::WriteHandle working on a ReadoutBuffer
    This allows writing system events to buffers instead of writing them
    directly to the listfile. This means buffers for the analysis side (snoop)
    look exactly the same as the ones written to the listfile.

---
+++ src/mesytec-mvlc/mvlc_listfile_util.h
@@ -0,0 +1,47 @@
+#ifndef __MESYTEC_MVLC_MVLC_LISTFILE_UTIL_H__
+#define __MESYTEC_MVLC_MVLC_LISTFILE_UTIL_H__
+
+#include <cstring>
+
+#include "mvlc_listfile.h"
+#include "util/readout_buffer.h"
+
+namespace mesytec
+{
+namespace mvlc
+{
+namespace listfile
+{
+
+// Implements the listfile::WriteHandle interface. Writes are directed to the
+// underlying ReadoutBuffer. The buffer is resized in write() if there's not
+// enough room available.
+class MESYTEC_MVLC_EXPORT BufferWriteHandle: public WriteHandle
+{
+    public:
+        BufferWriteHandle(ReadoutBuffer &buffer)
+            : m_buffer(buffer)
+        {
+        }
+
+        ~BufferWriteHandle() override {}
+
+        size_t write(const u8 *data, size_t size) override
+        {
+            m_buffer.ensureFreeSpace(size);
+            assert(m_buffer.free() >= size);
+            std::memcpy(m_buffer.data() + m_buffer.used(),
+                        data, size);
+            m_buffer.use(size);
+            return size;
+        }
+
+    private:
+        ReadoutBuffer &m_buffer;
+};
+
+} // end namespace listfile
+} // end namespace mvlc
+} // end namespace mesytec
+
+#endif /* __MESYTEC_MVLC_MVLC_LISTFILE_UTIL_H__ */
