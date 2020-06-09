4909db081152f0619ac4133d9c46450085d1448e
Florian Lüke <f.lueke@mesytec.com>
2020-06-09 09:35:12 +0200

    add filesystem utility functions based on minizip utilities

---
+++ src/mesytec-mvlc/util/filesystem.cc
@@ -0,0 +1,44 @@
+#include "util/filesystem.h"
+
+#include <cstdint>
+#include <memory>
+#include <mz.h>
+#include <mz_os.h>
+
+namespace mesytec
+{
+namespace mvlc
+{
+namespace util
+{
+
+std::string basename(const std::string &filepath)
+{
+    const char *filename = nullptr;
+
+    if (mz_path_get_filename(filepath.c_str(), &filename) == MZ_OK)
+        return std::string(filename);
+
+    return {};
+}
+
+std::string dirname(const std::string &filepath)
+{
+    auto buffer = std::make_unique<char[]>(filepath.size() + 1u);
+    std::copy(std::begin(filepath), std::end(filepath), buffer.get());
+    buffer[filepath.size()] = '\0';
+
+    if (mz_path_remove_filename(buffer.get()) == MZ_OK)
+        return std::string(buffer.get());
+
+    return {};
+}
+
+bool file_exists(const std::string &filepath)
+{
+    return mz_os_file_exists(filepath.c_str()) == MZ_OK;
+}
+
+}
+}
+}
