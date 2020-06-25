03e4bb2f037d3cbd007fbcbf6a3e0c21ad764f42
Florian Lüke <f.lueke@mesytec.com>
2020-06-25 13:14:04 +0200

    fix typo

--- src/mesytec-mvlc/mvlc_impl_usb.h
+++ src/mesytec-mvlc/mvlc_impl_usb.h
@@ -73,7 +73,7 @@ struct DeviceInfo
     struct Flags
     {
         // Set if the device is opened by some process at the time the info
-        // queried.
+        // is queried.
         static const u8 Opened = 1;
         static const u8 USB2   = 2;
         static const u8 USB3   = 4;
