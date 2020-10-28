69185280d09a73ee0c5eef7311524d47cfd3deb1
Florian Lüke <f.lueke@mesytec.com>
2020-10-28 14:32:51 +0100

    [eth] default to not disabling triggers when connecting

--- src/mesytec-mvlc/mvlc_impl_eth.h
+++ src/mesytec-mvlc/mvlc_impl_eth.h
@@ -176,7 +176,7 @@ class MESYTEC_MVLC_EXPORT Impl: public MVLCBasicInterface, public MVLC_ETH_Inter
         std::array<PipeStats, PipeCount> m_pipeStats;
         std::array<PacketChannelStats, NumPacketChannels> m_packetChannelStats;
         std::array<s32, NumPacketChannels> m_lastPacketNumbers;
-        bool m_disableTriggersOnConnect = true;
+        bool m_disableTriggersOnConnect = false;
         mutable TicketMutex m_statsMutex;
         mutable Protected<EthThrottleCounters> m_throttleCounters;
         Protected<EthThrottleContext> m_throttleContext;
