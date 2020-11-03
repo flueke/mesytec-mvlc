7f925d1537611d0c6a098eb7925e658d1895c55b
Florian Lüke <f.lueke@mesytec.com>
2020-11-03 15:47:39 +0100

    add fmt-7.1.1-12-g86bf6045 to externals

--- src/mesytec-mvlc/fmt.h
+++ src/mesytec-mvlc/fmt.h
@@ -1,6 +1,11 @@
 #ifndef __MESYTEC_MVLC_FMT_H__
 #define __MESYTEC_MVLC_FMT_H__
 
+#if 0
+#define FMT_HEADER_ONLY
 #include "fmt/format.h"
+#else
+#include <fmt/format.h>
+#endif
 
 #endif /* __MESYTEC_MVLC_FMT_H__ */
