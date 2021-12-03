499807b83620aaee34b3c19864572f56e9412d25
Florian Lüke <f.lueke@mesytec.com>
2021-12-03 15:23:42 +0100

    fix inline function dll export warning

--- src/mesytec-mvlc/event_builder.h
+++ src/mesytec-mvlc/event_builder.h
@@ -39,7 +39,7 @@ struct MESYTEC_MVLC_EXPORT IndexedTimestampFilterExtractor
         s32 index_;
 };
 
-MESYTEC_MVLC_EXPORT inline IndexedTimestampFilterExtractor make_mesytec_default_timestamp_extractor()
+inline IndexedTimestampFilterExtractor make_mesytec_default_timestamp_extractor()
 {
     return IndexedTimestampFilterExtractor(
         make_filter("11DDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"), // 30 bit non-extended timestamp
