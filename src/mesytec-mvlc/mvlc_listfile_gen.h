f8ca0446ea3babacc72ba83bc6793937affbfb69
Florian Lüke <f.lueke@mesytec.com>
2022-02-04 16:54:10 +0100

    listfile_gen fixes and tests

--- src/mesytec-mvlc/mvlc_listfile_gen.h
+++ src/mesytec-mvlc/mvlc_listfile_gen.h
@@ -1,9 +1,10 @@
 #ifndef __MESYTEC_MVLC_LISTFILE_GEN_H__
 #define __MESYTEC_MVLC_LISTFILE_GEN_H__
 
-#include "mesytec-mvlc/mvlc_constants.h"
+#include "mvlc_constants.h"
 #include "mvlc_listfile.h"
 #include "mvlc_readout_parser.h"
+#include "mvlc_util.h"
 #include "readout_buffer.h"
 
 namespace mesytec
@@ -27,226 +28,23 @@ size_t calculate_output_size(const readout_parser::ModuleData *moduleDataList, u
 }
 #endif
 
-static u32 FrameMaxWords = frame_headers::LengthMask;
-
 // Goal: write out all ModuleData blocks. Respect max frame sizes and use continuation frames
 // and continue bits accordingly.
 // Outer framing: F3 followed by optional F9 stack frames
 // Inner framing: F5 block read frames
 // All header types need to have the Continue bit if there is follow-up data.
-inline void write_module_data2(
+void write_module_data(
     ReadoutBuffer &dest, int crateIndex, int eventIndex,
-    const readout_parser::ModuleData *moduleDataList, unsigned moduleCount)
-{
-    assert(crateIndex <= frame_headers::CtrlIdMask);
-    // +1 because the standard readout stack for event 0 is stack 1
-    assert(eventIndex + 1 <= frame_headers::StackNumMask);
-
-
-    // Store header offsets from the start of the dest buffer. Stored pointers would get
-    // invalidated if the underlying buffer had to grow.
-    s64 stackHeaderOffset = -1; // offset to the current F3/F9 frame from dest.data() in bytes
-    s64 blockHeaderOffset = -1; // offset to the current F5 frame from dest.data() in bytes
-
-    // FIXME: using a small value for testing
-    //static const u32 FrameMaxWords = frame_headers::LengthMask;
-
-    u32 stackFrameWordsWritten = 0;
-    u32 blockFrameWordsWritten = 0;
-
-    auto get_cur_stack_header = [&dest, &stackHeaderOffset] () -> u32 *
-    {
-        assert(stackHeaderOffset >= 0);
-        if (stackHeaderOffset < 0) return nullptr;
-        auto addr = dest.data() + stackHeaderOffset;
-        assert(addr + sizeof(u32) < dest.data() + dest.capacity());
-        return reinterpret_cast<u32 *>(addr);
-    };
-
-    auto get_cur_block_header = [&dest, &blockHeaderOffset] () -> u32 *
-    {
-        assert(blockHeaderOffset >= 0);
-        if (blockHeaderOffset < 0) return nullptr;
-        auto addr = dest.data() + blockHeaderOffset;
-        assert(addr + sizeof(u32) < dest.data() + dest.capacity());
-        return reinterpret_cast<u32 *>(addr);
-    };
-
-    auto start_new_stack_frame = [&] (u8 frameType)
-    {
-        assert(blockHeaderOffset < 0);
-        u32 stackHeader = (frameType << frame_headers::TypeShift
-                           | (eventIndex + 1) << frame_headers::StackNumShift
-                           | crateIndex << frame_headers::CtrlIdShift
-                          );
-        dest.push_back(stackHeader);
-        stackHeaderOffset = dest.used() - sizeof(stackHeader);
-        assert(stackHeaderOffset >= 0);
-        stackFrameWordsWritten = 0;
-    };
-
-    auto start_new_block_frame = [&] ()
-    {
-        assert(stackHeaderOffset >= 0);
-        u32 blockHeader = (frame_headers::BlockRead << frame_headers::TypeShift);
-        dest.push_back(blockHeader);
-        blockHeaderOffset = dest.used() - sizeof(blockHeader);
-        assert(blockHeaderOffset >= 0);
-        blockFrameWordsWritten = 0;
-        ++stackFrameWordsWritten;
-    };
-
-    for (unsigned mi=0; mi<moduleCount; ++mi)
-    {
-        auto moduleData = moduleDataList[mi].data;
-        //const auto moduleBegin = moduleData.data;
-        const auto moduleEnd = moduleData.data + moduleData.size;
-        auto moduleIter = moduleData.data;
-
-        while (moduleIter != moduleEnd)
-        {
-            if (stackHeaderOffset < 0)
-                start_new_stack_frame(frame_headers::StackFrame);
-
-            if (blockHeaderOffset < 0)
-                start_new_block_frame();
-
-            while (moduleIter != moduleEnd
-                   && stackFrameWordsWritten < FrameMaxWords
-                   && blockFrameWordsWritten < FrameMaxWords)
-            {
-                dest.push_back(*moduleIter++);
-                ++blockFrameWordsWritten;
-                ++stackFrameWordsWritten;
-            }
-
-            *get_cur_block_header() |= blockFrameWordsWritten;
-
-            if (moduleIter != moduleEnd)
-            {
-                // Set Continue in the block frame header and close the frame.
-                *get_cur_block_header() |= frame_flags::Continue << frame_headers::FrameFlagsShift;
-                blockHeaderOffset = -1;
-
-                // Set Continue for the stack frame too and open a new StackContinuation frame.
-                *get_cur_stack_header() |= frame_flags::Continue << frame_headers::FrameFlagsShift;
-                *get_cur_stack_header() |= stackFrameWordsWritten; // size
-
-                stackHeaderOffset = -1;
-                start_new_stack_frame(frame_headers::StackContinuation);
-            }
-        }
-    }
-
-    if (stackHeaderOffset >= 0)
-    {
-        *get_cur_stack_header() |= stackFrameWordsWritten; // size
-    }
-}
-
-// Note: always emits a BlockRead frame for each module
-inline void write_module_data(ReadoutBuffer &dest, int crateIndex, int eventIndex,
-                       const readout_parser::ModuleData *moduleDataList, unsigned moduleCount)
-{
-    assert(crateIndex < frame_headers::CtrlIdMask);
-
-    // +1 because the standard readout stack for event 0 is stack 1
-    assert(eventIndex + 1 < frame_headers::StackNumMask);
-
-    size_t outputWordCount = 1u; // event header
-
-    for (unsigned mi=0;mi<moduleCount;++mi)
-    {
-        outputWordCount += 1; // module header
-        outputWordCount += moduleDataList[mi].data.size;
-    }
-
-    // -1 because the event header itself is not counted in the frames size field
-    assert(outputWordCount - 1 < frame_headers::LengthMask);
-
-    u32 eventHeader = (frame_headers::StackFrame << frame_headers::TypeShift
-                       | (eventIndex + 1) << frame_headers::StackNumShift
-                       | crateIndex << frame_headers::CtrlIdShift
-                       | (outputWordCount - 1)
-                      );
-
-    dest.push_back(eventHeader);
-
-    for (unsigned mi=0;mi<moduleCount;++mi)
-    {
-        const auto &moduleData = moduleDataList[mi].data;
-
-        assert(moduleData.size < frame_headers::LengthMask);
-
-        u32 moduleHeader = (frame_headers::BlockRead << frame_headers::TypeShift
-                            | moduleData.size
-                           );
-
-        dest.push_back(moduleHeader);
-
-        for (auto it=moduleData.data; it<moduleData.data + moduleData.size; ++it)
-            dest.push_back(*it);
-    }
-}
-
+    const readout_parser::ModuleData *moduleDataList, unsigned moduleCount,
+    u32 frameMaxWords = frame_headers::LengthMask);
 
 // Writes out an adjusted header containing the specified crateIndex, followed by the unmodified
 // system event data.
 //template<typename Dest>
 //void write_system_event(Dest &dest, int crateIndex, const u32 *header, u32 size)
-inline void write_system_event(ReadoutBuffer &dest, int crateIndex, const u32 *header, u32 size)
-{
-    assert(crateIndex < frame_headers::CtrlIdMask);
-    assert(size > 0);
-    assert(size < frame_headers::LengthMask);
-    assert(get_frame_type(*header) == frame_headers::SystemEvent);
-
-    u32 headerWithCrateIndex = *header | crateIndex << frame_headers::CtrlIdShift;
-
-    dest.push_back(headerWithCrateIndex);
-
-    for (auto it=header+1; it<header+size; ++it)
-        dest.push_back(*it);
-}
-
-
-#if 0
-namespace frame_headers
-{
-    enum FrameTypes: u8
-    {
-        BeginRun,
-        EndRun,
-        EventData,
-        ModuleData,
-        EndOfFile,
-        SystemEvent = 0xA,
-    };
-
-    // Event: Type | CrateIndex | EventIndex | Size
-    // Module: Type | ModuleIndex | Size
-    // SystemEvent: Type | CrateIndex | Size
-
-    static const u32 TypeShift = 28;
-    static const u32 TypeMask  = 0xf; // 4 bit type
-
-    static const u32 Index1Shift = 24;
-    static const u32 Index1Mask  = 0xf; // 4 bit CrateIndex/ModuleIndex
-
-    static const u32 Index2Shift = 20;
-    static const u32 Index2Mask  = 0xf; // 4 bit EventIndex
-
-    static const u32 FlagsShift  = 18;
-    static const u32 FlagsMask   = 0b11; // 2 bit flags
-
-    static const u32 SizeShift = 0u;
-    static const u32 SizeMask  = 0x3ffffu; // 18 bit size in 32-bit words
-}
-#endif
-
-/* Alternative to a new format: reuse the USB framing format.
- * This solution would allow to reuse the existing readout_parser and other tools.
- */
+void write_system_event(
+    ReadoutBuffer &dest, int crateIndex, const u32 *header, u32 size,
+    u32 frameMaxWords = frame_headers::LengthMask);
 
 }
 }
--- src/mesytec-mvlc/mvlc_listfile_gen.test.cc
+++ src/mesytec-mvlc/mvlc_listfile_gen.test.cc
@@ -1,53 +1,250 @@
 #include <gtest/gtest.h>
+#include "mvlc_constants.h"
 #include "mvlc_listfile_gen.h"
 #include "mvlc_buffer_validators.h"
+#include "mvlc_readout_parser.h"
 #include "util/io_util.h"
+#include "vme_constants.h"
 
 using namespace mesytec::mvlc;
 using namespace mesytec::mvlc::listfile;
 
-TEST(listfile_gen, First)
+namespace
 {
-    mesytec::mvlc::listfile::FrameMaxWords = 1;
+    std::vector<readout_parser::ModuleData> make_module_data_list(
+        const std::vector<std::vector<u32>> &dataStorage)
+    {
+        std::vector<readout_parser::ModuleData> moduleDataList;
+
+        for (auto &ds: dataStorage)
+        {
+            readout_parser::ModuleData md;
+            md.data.data = ds.data();
+            md.data.size = ds.size();
+            moduleDataList.emplace_back(md);
+        }
+
+        return moduleDataList;
+    }
+
+    void log_buffer(const ReadoutBuffer &buffer, bool verbose=true)
+    {
+        std::cout << "begin buffer" << std::endl;
+        for (u32 w: buffer.viewU32())
+        {
+            if (!verbose)
+                std::cout << fmt::format("  0x{:08X}", w) << std::endl;
+            else if (is_known_frame_header(w))
+                std::cout << fmt::format("  0x{:08X}, // {}", w, decode_frame_header(w)) << std::endl;
+            else
+                std::cout << fmt::format("  0x{:08X},", w) << std::endl;
+        }
+        std::cout << "end buffer" << std::endl;
+    }
+
+    using BufferView = nonstd::basic_string_view<const u32>;
+}
+
+TEST(listfile_gen, GenModuleDataAndParse)
+{
+    int crateIndex = 1;
+    int eventIndex = 2;
 
     std::vector<std::vector<u32>> dataStorage
     {
+        // module0
         {
             0x10000001,
             0x10000002,
         },
+        // module1
         {
             0x20000001,
             0x20000002,
         },
     };
 
-    std::vector<readout_parser::ModuleData> moduleDataList;
+    auto moduleDataList = make_module_data_list(dataStorage);
 
-    for (auto &ds: dataStorage)
+    StackCommandBuilder readoutStack;
+    readoutStack.beginGroup("1st module");
+    readoutStack.addVMEBlockRead(0, vme_amods::MBLT64, 0xffff);
+    readoutStack.beginGroup("2nd module");
+    readoutStack.addVMEBlockRead(0, vme_amods::MBLT64, 0xffff);
+
+    const std::vector<StackCommandBuilder> readoutStacks =
     {
-        readout_parser::ModuleData md;
-        md.data.data = ds.data();
-        md.data.size = ds.size();
-        moduleDataList.emplace_back(md);
+        {},             // eventIndex=0
+        {},             // eventIndex=1
+        readoutStack    // eventIndex=2
+    };
+
+    {
+        // Frames of max size 3 -> data from one module fits exactly into a single stack frame.
+        u32 frameMaxWords = 3;
+        ReadoutBuffer buffer;
+
+        write_module_data(
+            buffer, crateIndex, eventIndex,
+            moduleDataList.data(), moduleDataList.size(),
+            frameMaxWords);
+
+        log_buffer(buffer);
+        const std::vector<u32> expected =
+        {
+            0xF3832003, // StackResultFrame (len=3, stackNum=3, frameFlags=continue)
+            0xF5000002, // BlockReadFrame (len=2, frameFlags=none)
+            0x10000001,
+            0x10000002,
+            0xF9032003, // StackResultContinuation Frame (len=3, stackNum=3, frameFlags=none)
+            0xF5000002, // BlockReadFrame (len=2, frameFlags=none)
+            0x20000001,
+            0x20000002,
+        };
+
+        ASSERT_EQ(buffer.viewU32(), BufferView(expected.data(), expected.size()));
+
+        auto parserState = readout_parser::make_readout_parser(readoutStacks, crateIndex);
+        readout_parser::ReadoutParserCounters parserCounters = {};
+        readout_parser::ReadoutParserCallbacks parserCallbacks;
+
+        parserCallbacks.eventData = [&dataStorage] (
+            void *, int crateIndex, int eventIndex,
+            const readout_parser::ModuleData *moduleDataList, unsigned moduleCount)
+        {
+            std::cout << fmt::format("eventData callback: ci={}, ei={}, moduleCount={}",
+                                     crateIndex, eventIndex, moduleCount) << std::endl;
+
+            ASSERT_EQ(crateIndex, 1);
+            ASSERT_EQ(eventIndex, 2);
+            ASSERT_EQ(moduleCount, 2);
+
+            for (unsigned mi=0; mi<moduleCount; ++mi)
+            {
+                BufferView modView(moduleDataList[mi].data.data, moduleDataList[mi].data.size);
+                BufferView expected(dataStorage[mi].data(), dataStorage[mi].size());
+
+                ASSERT_EQ(modView, expected);
+            }
+        };
+
+        auto pr = readout_parser::parse_readout_buffer(
+            static_cast<ConnectionType>(buffer.type()),
+            parserState,
+            parserCallbacks,
+            parserCounters,
+            buffer.bufferNumber(), buffer.viewU32().data(), buffer.viewU32().size()
+            );
+
+        std::cout << "parseresult: " << get_parse_result_name(pr) << std::endl;
+
+        ASSERT_EQ(pr, readout_parser::ParseResult::Ok);
     }
 
-    ReadoutBuffer buffer;
+    {
+        // Shorted possible frames that should still work -> one word of module data per
+        // BlockReadFrame
+        u32 frameMaxWords = 2;
+        ReadoutBuffer buffer;
+        write_module_data(
+            buffer, crateIndex, eventIndex,
+            moduleDataList.data(), moduleDataList.size(),
+            frameMaxWords);
+
+        log_buffer(buffer);
+        const std::vector<u32> expected =
+        {
+            0xF3832002, // StackResultFrame (len=2, stackNum=3, frameFlags=continue)
+            0xF5800001, // BlockReadFrame (len=1, frameFlags=continue)
+            0x10000001,
+            0xF9832002, // StackResultContinuation Frame (len=2, stackNum=3, frameFlags=continue)
+            0xF5000001, // BlockReadFrame (len=1, frameFlags=none)
+            0x10000002,
+            0xF9832002, // StackResultContinuation Frame (len=2, stackNum=3, frameFlags=continue)
+            0xF5800001, // BlockReadFrame (len=1, frameFlags=continue)
+            0x20000001,
+            0xF9032002, // StackResultContinuation Frame (len=2, stackNum=3, frameFlags=none)
+            0xF5000001, // BlockReadFrame (len=1, frameFlags=none)
+            0x20000002,
+        };
+
+        ASSERT_EQ(buffer.viewU32(), BufferView(expected.data(), expected.size()));
+
+        auto parserState = readout_parser::make_readout_parser(readoutStacks, crateIndex);
+        readout_parser::ReadoutParserCounters parserCounters = {};
+        readout_parser::ReadoutParserCallbacks parserCallbacks;
+
+        parserCallbacks.eventData = [&dataStorage] (
+            void *, int crateIndex, int eventIndex,
+            const readout_parser::ModuleData *moduleDataList, unsigned moduleCount)
+        {
+            std::cout << fmt::format("eventData callback: ci={}, ei={}, moduleCount={}",
+                                     crateIndex, eventIndex, moduleCount) << std::endl;
+
+            ASSERT_EQ(crateIndex, 1);
+            ASSERT_EQ(eventIndex, 2);
+            ASSERT_EQ(moduleCount, 2);
+
+            for (unsigned mi=0; mi<moduleCount; ++mi)
+            {
+                BufferView modView(moduleDataList[mi].data.data, moduleDataList[mi].data.size);
+                BufferView expected(dataStorage[mi].data(), dataStorage[mi].size());
+
+                ASSERT_EQ(modView, expected);
+            }
+        };
+
+        auto pr = readout_parser::parse_readout_buffer(
+            static_cast<ConnectionType>(buffer.type()),
+            parserState,
+            parserCallbacks,
+            parserCounters,
+            buffer.bufferNumber(), buffer.viewU32().data(), buffer.viewU32().size()
+            );
+
+        std::cout << "parseresult: " << get_parse_result_name(pr) << std::endl;
+
+        ASSERT_EQ(pr, readout_parser::ParseResult::Ok);
+    }
+}
+
+TEST(listfile_gen, GenSystemEventAndParse)
+{
     int crateIndex = 1;
-    int eventIndex = 2;
 
-    write_module_data2(buffer, crateIndex, eventIndex, moduleDataList.data(), moduleDataList.size());
+    // Note: not adding the crateIndex here because currently the SystemEvent producing code
+    // (ReadoutWorker) does not know the crateIndex and thus leaves the field cleared.
+    u32 systemEventHeader = (frame_headers::SystemEvent << frame_headers::TypeShift);
 
-    //util::log_buffer(std::cout, buffer.viewU32());
-
-    std::cout << "begin buffer" << std::endl;
-    for (u32 w: buffer.viewU32())
+    std::vector<u32> eventStorage =
     {
-        auto ft = get_frame_type(w);
-        if (is_known_frame_header(w))
-            std::cout << fmt::format("  0x{:08X} -> {}", w, decode_frame_header(w)) << std::endl;
-        else
-            std::cout << fmt::format("  0x{:08X}", w) << std::endl;
+        systemEventHeader,
+        0x10000001,
+        0x10000002,
+        0x10000003,
+    };
+
+    {
+        u32 frameMaxWords = 3;
+        ReadoutBuffer buffer;
+
+        write_system_event(
+            buffer, crateIndex, eventStorage.data(), eventStorage.size(),
+            frameMaxWords);
+
+        log_buffer(buffer);
     }
-    std::cout << "end buffer" << std::endl;
+
+    {
+        u32 frameMaxWords = 2;
+        ReadoutBuffer buffer;
+
+        write_system_event(
+            buffer, crateIndex, eventStorage.data(), eventStorage.size(),
+            frameMaxWords);
+
+        log_buffer(buffer);
+    }
+
+    // XXX: leftoff here: check the buffers!
 }
