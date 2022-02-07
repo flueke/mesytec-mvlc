07df62c4118b618f64e1e2b66fdd2b14c0d5d2af
Florian Lüke <f.lueke@mesytec.com>
2022-02-07 12:21:25 +0100

    listfile_gen: handle length field by zeroing it out and setting it again
    Fixes assertion when copying SystemEvents which will have the length set
    in their header already.

--- src/mesytec-mvlc/mvlc_listfile_gen.h
+++ src/mesytec-mvlc/mvlc_listfile_gen.h
@@ -14,34 +14,17 @@ namespace mvlc
 namespace listfile
 {
 
-#if 0
-size_t calculate_output_size(const readout_parser::ModuleData *moduleDataList, unsigned moduleCount)
-{
-    size_t res = 1u; // first StackFrame
+// Listfile generation from unpacked/parsed module readout data and system event data.
+// The generated format is compatible to the MVLC_USB framing format and it should be compatible
+// with the existing readout_parser module.
+//
+// The crateIndex argument is used for the CtrlId field in the frame headers.
 
-    for (unsigned mi=0; mi<moduleCount;++mi)
-    {
-
-    }
-
-    return res;
-}
-#endif
-
-// Goal: write out all ModuleData blocks. Respect max frame sizes and use continuation frames
-// and continue bits accordingly.
-// Outer framing: F3 followed by optional F9 stack frames
-// Inner framing: F5 block read frames
-// All header types need to have the Continue bit if there is follow-up data.
 void write_module_data(
     ReadoutBuffer &dest, int crateIndex, int eventIndex,
     const readout_parser::ModuleData *moduleDataList, unsigned moduleCount,
     u32 frameMaxWords = frame_headers::LengthMask);
 
-// Writes out an adjusted header containing the specified crateIndex, followed by the unmodified
-// system event data.
-//template<typename Dest>
-//void write_system_event(Dest &dest, int crateIndex, const u32 *header, u32 size)
 void write_system_event(
     ReadoutBuffer &dest, int crateIndex, const u32 *header, u32 size,
     u32 frameMaxWords = frame_headers::LengthMask);
