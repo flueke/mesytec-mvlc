db17d4aa1db6a98d1b085e2a25470d513f0aafea
Florian Lüke <f.lueke@mesytec.com>
2023-03-22 11:32:41 +0100

    vme-scan-bus: more options and better output/log formatting

--- extras/dev-tools/vme_scan_bus.cc
+++ extras/dev-tools/vme_scan_bus.cc
@@ -108,16 +108,27 @@ inline bool is_mdpp16(u16 hwId) { return hwId == HardwareIds::MDPP_16; }
 inline bool is_mdpp32(u16 hwId) { return hwId == HardwareIds::MDPP_32; }
 inline bool is_mdpp(u16 hwId) { return is_mdpp16(hwId) || is_mdpp32(hwId); }
 
-// Scans the uppper 64k addresses for mesytec modules. Returns a list of
-// candidate addresses.
-std::vector<u32> scan_vme_bus_for_candidates(MVLC &mvlc)
+// Scans the addresses in the range [scanBaseBegin, scanBaseEnd) for (mesytec)
+// vme modules. scanBaseBegin/End specify the upper 16 bits of the full 32-bit
+// vme address.
+// Returns a list of candidate addresses (addresses where the read was
+// successful or resulted in BERR).
+std::vector<u32> scan_vme_bus_for_candidates(
+    MVLC &mvlc,
+    const u16 scanBaseBegin = 0u,
+    const u16 scanBaseEnd   = 0xffffu,
+    const u16 probeRegister = 0u,
+    const u8 probeAmod = vme_amods::A32,
+    const VMEDataWidth probeDataWidth = VMEDataWidth::D16)
 {
     std::vector<u32> response;
     std::vector<u32> result;
 
     // Note: 0xffff itself is never checked as that is taken by the MVLC itself.
-    const u32 baseMax = 0xffffu;
-    u32 base = 0;
+    const u32 baseMax = scanBaseEnd;
+    u32 base = scanBaseBegin;
+    size_t nStacks = 0u;
+    auto tStart = std::chrono::steady_clock::now();
 
     do
     {
@@ -129,7 +140,7 @@ std::vector<u32> scan_vme_bus_for_candidates(MVLC &mvlc)
                 && base < baseMax)
         {
             u32 readAddress = (base << 16) | (ProbeRegister & 0xffffu);
-            sb.addVMERead(readAddress, vme_amods::A32, VMEDataWidth::D16);
+            sb.addVMERead(readAddress, probeAmod, probeDataWidth);
             ++base;
         }
 
@@ -138,6 +149,8 @@ std::vector<u32> scan_vme_bus_for_candidates(MVLC &mvlc)
 
         if (auto ec = mvlc.stackTransaction(sb, response))
             throw std::system_error(ec);
+
+        ++nStacks;
 
         spdlog::trace("Stack result for baseStart=0x{:04x}, baseEnd=0x{:04x} (#addrs={}), response.size()={}\n",
             baseStart, base, base-baseStart, response.size());
@@ -154,7 +167,6 @@ std::vector<u32> scan_vme_bus_for_candidates(MVLC &mvlc)
                 spdlog::warn("MVLC stack execution returned a syntax error. Scanbus results may be incomplete!");
             }
         }
-
 
         // +2 to skip over 0xF3 and the marker
         for (auto it = std::begin(response) + 2; it < std::end(response); ++it)
@@ -176,6 +188,11 @@ std::vector<u32> scan_vme_bus_for_candidates(MVLC &mvlc)
         response.clear();
 
     } while (base < baseMax);
+
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>((std::chrono::steady_clock::now() - tStart));
+
+    spdlog::info("Scanned {} addresses in {} ms using {} stack transactions",
+        scanBaseEnd - scanBaseBegin, elapsed.count(), nStacks);
 
     return result;
 }
@@ -207,6 +224,12 @@ int main(int argc, char *argv[])
     bool opt_logTrace = false;
     std::string opt_mvlcEthHost;
     bool opt_mvlcUseFirstUSBDevice = true;
+    std::string opt_scanBaseBegin   = "0",
+                opt_scanBaseEnd     = "0xffff",
+                opt_probeRegister   = "0",
+                opt_probeAmod       = "0x09",
+                opt_probeDataWidth  = "d16"
+                ;
 
     auto cli
         = lyra::help(opt_showHelp)
@@ -220,6 +243,12 @@ int main(int argc, char *argv[])
         | lyra::opt(opt_logDebug)["--debug"]("enable debug logging")
 
         | lyra::opt(opt_logTrace)["--trace"]("enable trace logging")
+
+        | lyra::opt(opt_scanBaseBegin, "addr")["--scan-begin"]("first scan base address")
+        | lyra::opt(opt_scanBaseEnd, "addr")["--scan-end"]("one past last scan base address")
+        | lyra::opt(opt_probeRegister, "addr")["--probe-register"]("register address to probe (low 16 bits of 32 bit vme address)")
+        | lyra::opt(opt_probeAmod, "amod")["--probe-amod"]("vme amod to use when probing (defaults to 0x09)")
+        | lyra::opt(opt_probeDataWidth, "dataWidth")["--probe-datawidth"]("vme datawidth to use when probing (d16|d32)")
         ;
 
     auto cliParseResult = cli.parse({ argc, argv });
@@ -232,8 +261,18 @@ int main(int argc, char *argv[])
 
     if (opt_showHelp)
     {
-        cout << cli << endl;
+        cout << "mvlc vme-scan-bus: Scans the upper 64k VME bus addresses for active modules.\n"
+                "                   Reports module type, firmware type and firmware revision for mesytec modules.\n"
+             << cli << "\n";
         return 0;
+    }
+
+
+    set_global_log_level(spdlog::level::warn);
+    if (auto defaultLogger = spdlog::default_logger())
+    {
+        defaultLogger->set_level(spdlog::level::info);
+        defaultLogger->set_pattern("%v");
     }
 
     // logging setup
@@ -242,6 +281,29 @@ int main(int argc, char *argv[])
 
     if (opt_logTrace)
         set_global_log_level(spdlog::level::trace);
+
+    u32 scanBaseBegin = std::stoul(opt_scanBaseBegin, nullptr, 0);
+    u32 scanBaseEnd = std::stoul(opt_scanBaseEnd, nullptr, 0);
+    u32 probeRegister = std::stoull(opt_probeRegister, nullptr, 0);
+    u8 probeAmod = std::stoul(opt_probeAmod, nullptr, 0);
+
+    if (scanBaseBegin > scanBaseEnd)
+        std::swap(scanBaseBegin, scanBaseEnd);
+
+    auto probeDataWidth = VMEDataWidth::D16;
+
+    if (opt_probeDataWidth == "d16")
+        probeDataWidth = VMEDataWidth::D16;
+    else if (opt_probeDataWidth == "d32")
+        probeDataWidth = VMEDataWidth::D32;
+    else
+    {
+        cerr << "Error parsing --probe-datawidth, expected d16|d32\n";
+        return 1;
+    }
+
+    spdlog::info("Scan range: [{:#06x}, {:#06x}), {} addresses, probeRegister={:#06x}, probeAmod={:#04x}, probeDataWidth={}",
+        scanBaseBegin, scanBaseEnd, scanBaseEnd - scanBaseBegin, probeRegister, probeAmod, opt_probeDataWidth);
 
     try
     {
@@ -258,10 +320,13 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        if (auto candidates = scan_vme_bus_for_candidates(mvlc);
-            !candidates.empty())
+        auto candidates = scan_vme_bus_for_candidates(
+            mvlc, scanBaseBegin, scanBaseEnd,
+            probeRegister, probeAmod, probeDataWidth);
+
+        if (!candidates.empty())
         {
-            spdlog::debug("Found module candidate addresses: {:#010x}", fmt::join(candidates, ", "));
+            spdlog::info("Found {} module candidate addresses: {:#010x}", candidates.size(), fmt::join(candidates, ", "));
 
             for (auto addr: candidates)
             {
