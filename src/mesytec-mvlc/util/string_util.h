a9d6a07d85d8e64bec5da10170eae4fbf40ab541
Florian Lüke <f.lueke@mesytec.com>
2023-09-05 12:09:37 +0200

    libmvp: move string utils into mesytec-mvlc

--- src/mesytec-mvlc/util/string_util.h
+++ src/mesytec-mvlc/util/string_util.h
@@ -36,6 +36,32 @@ inline std::string str_tolower(std::string s)
     return s;
 }
 
+// Helper for unindenting raw string literals.
+// https://stackoverflow.com/a/24900770
+inline std::string unindent(const char* p)
+{
+    std::string result;
+    if (*p == '\n') ++p;
+    const char* p_leading = p;
+    while (std::isspace(*p) && *p != '\n')
+        ++p;
+    size_t leading_len = p - p_leading;
+    while (*p)
+    {
+        result += *p;
+        if (*p++ == '\n')
+        {
+            for (size_t i = 0; i < leading_len; ++i)
+                if (p[i] != p_leading[i])
+                    goto dont_skip_leading;
+            p += leading_len;
+        }
+      dont_skip_leading: ;
+    }
+    return result;
+}
+
+
 } // end namespace util
 } // end namespace mvlc
 } // end namespace mesytec
