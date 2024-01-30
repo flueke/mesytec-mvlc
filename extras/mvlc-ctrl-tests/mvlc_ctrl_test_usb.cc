44ab518a9062081c36b6f4898690f2a3b47650a9
Florian Lüke <f.lueke@mesytec.com>
2024-01-30 12:42:27 +0100

    fix code formatting

--- extras/mvlc-ctrl-tests/mvlc_ctrl_test_usb.cc
+++ extras/mvlc-ctrl-tests/mvlc_ctrl_test_usb.cc
@@ -85,73 +85,73 @@ TEST(MvlcUsb, ReadRegister)
     ASSERT_FALSE(ec) << ec.message();
     ASSERT_TRUE(mvlc.isConnected());
 
-for (size_t i=0; i<1'000'000; ++i)
-{
-    SuperCommandBuilder cmdList;
-    cmdList.addReferenceWord(i); // XXX: Makes the response one word larger. 15 bytes in total now!
-    cmdList.addReadLocal(registers::hardware_id);
-    auto request = make_command_buffer(cmdList);
+    for (size_t i = 0; i < 1'000'000; ++i)
+    {
+        SuperCommandBuilder cmdList;
+        cmdList.addReferenceWord(i); // XXX: Makes the response one word larger. 15 bytes in total now!
+        cmdList.addReadLocal(registers::hardware_id);
+        auto request = make_command_buffer(cmdList);
 
-    spdlog::info("request={:#010x}", fmt::join(request, ", "));
-    size_t bytesWritten = 0u;
-    const size_t bytesToWrite = request.size() * sizeof(u32);
-    ec = mvlc.write(Pipe::Command, reinterpret_cast<const u8 *>(request.data()), bytesToWrite, bytesWritten);
+        spdlog::info("request={:#010x}", fmt::join(request, ", "));
+        size_t bytesWritten = 0u;
+        const size_t bytesToWrite = request.size() * sizeof(u32);
+        ec = mvlc.write(Pipe::Command, reinterpret_cast<const u8 *>(request.data()), bytesToWrite, bytesWritten);
 
-    ASSERT_FALSE(ec) << ec.message();
-    ASSERT_EQ(bytesToWrite, bytesWritten);
+        ASSERT_FALSE(ec) << ec.message();
+        ASSERT_EQ(bytesToWrite, bytesWritten);
 
-    //std::this_thread::sleep_for(std::chrono::microseconds(100));
+        // std::this_thread::sleep_for(std::chrono::microseconds(100));
 
-    // Linux: At this point the read timeout has been set to 0 at the end of
-    // connect(). Reading small amounts of data immediately returns FT_TIMEOUT
-    // and 0 bytes read. Starting from 1024 * 128, most times the expected
-    // result of 12 bytes is returned, but not always.
-    // The current APIv2 implementation doesn't run into problems because it
-    // just reads in a loop.
+        // Linux: At this point the read timeout has been set to 0 at the end of
+        // connect(). Reading small amounts of data immediately returns FT_TIMEOUT
+        // and 0 bytes read. Starting from 1024 * 128, most times the expected
+        // result of 12 bytes is returned, but not always.
+        // The current APIv2 implementation doesn't run into problems because it
+        // just reads in a loop.
 
 #ifdef __linux__
-    auto ec = usb::set_endpoint_timeout(mvlc.getHandle(), usb::get_endpoint(Pipe::Command, usb::EndpointDirection::In), 1000);
-    ASSERT_FALSE(ec) << ec.message();
+        auto ec = usb::set_endpoint_timeout(mvlc.getHandle(), usb::get_endpoint(Pipe::Command, usb::EndpointDirection::In), 1000);
+        ASSERT_FALSE(ec) << ec.message();
 #endif
 
-    static const size_t responseCapacityInBytes = 4 * sizeof(u32);
-    std::vector<u32> response(responseCapacityInBytes / sizeof(u32));
-    const size_t responseCapacity = response.size() * sizeof(u32);
-    size_t bytesRead = 0u;
-    size_t retryCount = 0u;
-    static const size_t ReadRetryMaxCount = 20;
-    auto tReadTotalStart = std::chrono::steady_clock::now();
+        static const size_t responseCapacityInBytes = 4 * sizeof(u32);
+        std::vector<u32> response(responseCapacityInBytes / sizeof(u32));
+        const size_t responseCapacity = response.size() * sizeof(u32);
+        size_t bytesRead = 0u;
+        size_t retryCount = 0u;
+        static const size_t ReadRetryMaxCount = 20;
+        auto tReadTotalStart = std::chrono::steady_clock::now();
 
-    while (retryCount < ReadRetryMaxCount)
-    {
-        auto tReadStart = std::chrono::steady_clock::now();
-        ec = mvlc.read(Pipe::Command, reinterpret_cast<u8 *>(response.data()), responseCapacity, bytesRead);
-        auto tReadEnd = std::chrono::steady_clock::now();
-        auto readElapsed = std::chrono::duration_cast<std::chrono::microseconds>(tReadEnd - tReadStart);
-        spdlog::info("read(): ec={}, bytesRequested={}, bytesRead={}, read took {} µs", ec.message(), responseCapacity, bytesRead, readElapsed.count());
+        while (retryCount < ReadRetryMaxCount)
+        {
+            auto tReadStart = std::chrono::steady_clock::now();
+            ec = mvlc.read(Pipe::Command, reinterpret_cast<u8 *>(response.data()), responseCapacity, bytesRead);
+            auto tReadEnd = std::chrono::steady_clock::now();
+            auto readElapsed = std::chrono::duration_cast<std::chrono::microseconds>(tReadEnd - tReadStart);
+            spdlog::info("read(): ec={}, bytesRequested={}, bytesRead={}, read took {} µs", ec.message(), responseCapacity, bytesRead, readElapsed.count());
 
-        if (ec != ErrorType::Timeout)
-            break;
+            if (ec != ErrorType::Timeout)
+                break;
 
-        spdlog::warn("read() timed out, retrying!");
-        ++retryCount;
+            spdlog::warn("read() timed out, retrying!");
+            ++retryCount;
+        }
+
+        ASSERT_FALSE(ec) << ec.message();
+        ASSERT_TRUE(bytesRead % sizeof(u32) == 0);
+        const size_t wordsRead = bytesRead / sizeof(u32);
+        response.resize(wordsRead);
+        spdlog::info("response={:#010x}", fmt::join(response, ", "));
+        ASSERT_EQ(wordsRead, 4);
+        ASSERT_EQ(response[1] & 0xffffu, i & 0xffffu);
+        ASSERT_EQ(response[3], 0x5008u); // mvlc hardware id
+
+        if (!ec && retryCount > 1)
+        {
+            auto tReadTotalEnd = std::chrono::steady_clock::now();
+            auto readTotalElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(tReadTotalEnd - tReadTotalStart);
+            spdlog::warn("read() succeeded after {} retries, total read time {} ms, cycle #{}", retryCount, readTotalElapsed.count(), i);
+            return;
+        }
     }
-
-    ASSERT_FALSE(ec) << ec.message();
-    ASSERT_TRUE(bytesRead % sizeof(u32) == 0);
-    const size_t wordsRead = bytesRead / sizeof(u32);
-    response.resize(wordsRead);
-    spdlog::info("response={:#010x}", fmt::join(response, ", "));
-    ASSERT_EQ(wordsRead, 4);
-    ASSERT_EQ(response[1] & 0xffffu, i & 0xffffu);
-    ASSERT_EQ(response[3], 0x5008u); // mvlc hardware id
-
-    if (!ec && retryCount > 1)
-    {
-        auto tReadTotalEnd = std::chrono::steady_clock::now();
-        auto readTotalElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(tReadTotalEnd - tReadTotalStart);
-        spdlog::warn("read() succeeded after {} retries, total read time {} ms, cycle #{}", retryCount, readTotalElapsed.count(), i);
-        return;
-    }
-}
 }
