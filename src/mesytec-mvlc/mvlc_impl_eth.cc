6621dafa26338c98f76eb12fc9f72812f0ca48b8
Florian Lüke <f.lueke@mesytec.com>
2024-04-18 16:52:15 +0200

    eth: make socket utility functions public
    TODO: move to a socket abstraction library. Same/similary code exists in
    mesytec-mcpd.

--- src/mesytec-mvlc/mvlc_impl_eth.cc
+++ src/mesytec-mvlc/mvlc_impl_eth.cc
@@ -50,61 +50,14 @@
 #define MVLC_ETH_THROTTLE_WRITE_DEBUG_FILE 0
 #endif
 
+using namespace mesytec::mvlc;
+
 namespace
 {
-using namespace mesytec::mvlc;
 
 static const unsigned DefaultWriteTimeout_ms = 500;
 static const unsigned DefaultReadTimeout_ms  = 500;
 
-
-// Does IPv4 host lookup for a UDP socket. On success the resulting struct
-// sockaddr_in is copied to dest.
-std::error_code lookup(const std::string &host, u16 port, sockaddr_in &dest)
-{
-    using namespace mesytec::mvlc;
-
-    if (host.empty())
-        return MVLCErrorCode::EmptyHostname;
-
-    dest = {};
-    struct addrinfo hints = {};
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_protocol = IPPROTO_UDP;
-
-    struct addrinfo *result = nullptr, *rp = nullptr;
-
-    int rc = getaddrinfo(host.c_str(), std::to_string(port).c_str(),
-                         &hints, &result);
-
-    // TODO: check getaddrinfo specific error codes. make and use getaddrinfo error category
-    if (rc != 0)
-    {
-        //qDebug("%s: HostLookupError, host=%s, error=%s", __PRETTY_FUNCTION__, host.c_str(),
-        //       gai_strerror(rc));
-        return make_error_code(MVLCErrorCode::HostLookupError);
-    }
-
-    for (rp = result; rp != NULL; rp = rp->ai_next)
-    {
-        if (rp->ai_addrlen == sizeof(dest))
-        {
-            std::memcpy(&dest, rp->ai_addr, rp->ai_addrlen);
-            break;
-        }
-    }
-
-    freeaddrinfo(result);
-
-    if (!rp)
-    {
-        //qDebug("%s: HostLookupError, host=%s, no result found", __PRETTY_FUNCTION__, host.c_str());
-        return make_error_code(MVLCErrorCode::HostLookupError);
-    }
-
-    return {};
-}
 
 struct timeval ms_to_timeval(unsigned ms)
 {
@@ -190,67 +143,6 @@ inline std::string format_ipv4(u32 a)
 // UDP header is 8 bytes
 static const size_t MaxOutgoingPayloadSize = 1500 - 20 - 8;
 
-// Note: it is not necessary to split writes into multiple calls to send()
-// because outgoing MVLC command buffers have to be smaller than the maximum,
-// non-jumbo ethernet MTU.
-// The send() call should return EMSGSIZE if the payload is too large to be
-// atomically transmitted.
-#ifdef __WIN32
-inline std::error_code write_to_socket(
-    int socket, const u8 *buffer, size_t size, size_t &bytesTransferred)
-{
-    assert(size <= MaxOutgoingPayloadSize);
-
-    bytesTransferred = 0;
-
-    ssize_t res = ::send(socket, reinterpret_cast<const char *>(buffer), size, 0);
-
-    if (res == SOCKET_ERROR)
-    {
-        int err = WSAGetLastError();
-
-        if (err == WSAETIMEDOUT || err == WSAEWOULDBLOCK)
-            return make_error_code(MVLCErrorCode::SocketWriteTimeout);
-
-        // Maybe TODO: use WSAGetLastError here with a WSA specific error
-        // category like this: https://gist.github.com/bbolli/710010adb309d5063111889530237d6d
-        return make_error_code(MVLCErrorCode::SocketError);
-    }
-
-    bytesTransferred = res;
-    return {};
-}
-#else // !__WIN32
-inline std::error_code write_to_socket(
-    int socket, const u8 *buffer, size_t size, size_t &bytesTransferred)
-{
-    if (size > MaxOutgoingPayloadSize)
-    {
-        get_logger("mvlc_eth")->error("write_to_socket: UdpMaxOutgoingPacketSizeExceeded: size={}, max={}",
-            size, MaxOutgoingPayloadSize);
-        // Assertion to catch it in debug builds, error code returned in release builds.
-        assert(size <= MaxOutgoingPayloadSize);
-        return make_error_code(MVLCErrorCode::UdpMaxOutgoingPacketSizeExceeded);
-    }
-
-    bytesTransferred = 0;
-
-    ssize_t res = ::send(socket, reinterpret_cast<const char *>(buffer), size, 0);
-
-    if (res < 0)
-    {
-        if (errno == EAGAIN || errno == EWOULDBLOCK)
-            return make_error_code(MVLCErrorCode::SocketWriteTimeout);
-
-        return std::error_code(errno, std::system_category());
-    }
-
-    bytesTransferred = res;
-    return {};
-}
-#endif // !__WIN32
-
-
 // Amount of receive buffer space requested from the OS for both the command
 // and data sockets. It's not considered an error if less buffer space is
 // granted.
@@ -282,7 +174,7 @@ std::error_code send_delay_command(int delaySock, u16 delay_us)
     cmd |= delay_us;
 
     size_t bytesTransferred = 0;
-    auto ec = write_to_socket(delaySock, reinterpret_cast<const u8 *>(&cmd), sizeof(cmd), bytesTransferred);
+    auto ec = eth::write_to_socket(delaySock, reinterpret_cast<const u8 *>(&cmd), sizeof(cmd), bytesTransferred);
 
     if (ec)
         return ec;
@@ -705,6 +597,182 @@ namespace mvlc
 namespace eth
 {
 
+// Does IPv4 host lookup for a UDP socket. On success the resulting struct
+// sockaddr_in is copied to dest.
+std::error_code lookup(const std::string &host, u16 port, sockaddr_in &dest)
+{
+    using namespace mesytec::mvlc;
+
+    if (host.empty())
+        return MVLCErrorCode::EmptyHostname;
+
+    dest = {};
+    struct addrinfo hints = {};
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
+
+    struct addrinfo *result = nullptr, *rp = nullptr;
+
+    int rc = getaddrinfo(host.c_str(), std::to_string(port).c_str(),
+                         &hints, &result);
+
+    // TODO: check getaddrinfo specific error codes. make and use getaddrinfo error category
+    if (rc != 0)
+    {
+        //qDebug("%s: HostLookupError, host=%s, error=%s", __PRETTY_FUNCTION__, host.c_str(),
+        //       gai_strerror(rc));
+        return make_error_code(MVLCErrorCode::HostLookupError);
+    }
+
+    for (rp = result; rp != NULL; rp = rp->ai_next)
+    {
+        if (rp->ai_addrlen == sizeof(dest))
+        {
+            std::memcpy(&dest, rp->ai_addr, rp->ai_addrlen);
+            break;
+        }
+    }
+
+    freeaddrinfo(result);
+
+    if (!rp)
+    {
+        //qDebug("%s: HostLookupError, host=%s, no result found", __PRETTY_FUNCTION__, host.c_str());
+        return make_error_code(MVLCErrorCode::HostLookupError);
+    }
+
+    return {};
+}
+
+// Note: it is not necessary to split writes into multiple calls to send()
+// because outgoing MVLC command buffers have to be smaller than the maximum,
+// non-jumbo ethernet MTU.
+// The send() call should return EMSGSIZE if the payload is too large to be
+// atomically transmitted.
+#ifdef __WIN32
+std::error_code write_to_socket(
+    int socket, const u8 *buffer, size_t size, size_t &bytesTransferred)
+{
+    assert(size <= MaxOutgoingPayloadSize);
+
+    bytesTransferred = 0;
+
+    ssize_t res = ::send(socket, reinterpret_cast<const char *>(buffer), size, 0);
+
+    if (res == SOCKET_ERROR)
+    {
+        int err = WSAGetLastError();
+
+        if (err == WSAETIMEDOUT || err == WSAEWOULDBLOCK)
+            return make_error_code(MVLCErrorCode::SocketWriteTimeout);
+
+        // Maybe TODO: use WSAGetLastError here with a WSA specific error
+        // category like this: https://gist.github.com/bbolli/710010adb309d5063111889530237d6d
+        return make_error_code(MVLCErrorCode::SocketError);
+    }
+
+    bytesTransferred = res;
+    return {};
+}
+#else // !__WIN32
+std::error_code write_to_socket(
+    int socket, const u8 *buffer, size_t size, size_t &bytesTransferred)
+{
+    if (size > MaxOutgoingPayloadSize)
+    {
+        get_logger("mvlc_eth")->error("write_to_socket: UdpMaxOutgoingPacketSizeExceeded: size={}, max={}",
+            size, MaxOutgoingPayloadSize);
+        // Assertion to catch it in debug builds, error code returned in release builds.
+        assert(size <= MaxOutgoingPayloadSize);
+        return make_error_code(MVLCErrorCode::UdpMaxOutgoingPacketSizeExceeded);
+    }
+
+    bytesTransferred = 0;
+
+    ssize_t res = ::send(socket, reinterpret_cast<const char *>(buffer), size, 0);
+
+    if (res < 0)
+    {
+        if (errno == EAGAIN || errno == EWOULDBLOCK)
+            return make_error_code(MVLCErrorCode::SocketWriteTimeout);
+
+        return std::error_code(errno, std::system_category());
+    }
+
+    bytesTransferred = res;
+    return {};
+}
+#endif // !__WIN32
+
+#ifdef __WIN32
+std::error_code receive_one_packet(int sockfd, u8 *dest, size_t size,
+                                                 u16 &bytesTransferred, int timeout_ms)
+{
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(sockfd, &fds);
+
+    struct timeval tv = ms_to_timeval(timeout_ms);
+
+    int sres = ::select(0, &fds, nullptr, nullptr, &tv);
+
+    if (sres == 0)
+        return make_error_code(MVLCErrorCode::SocketReadTimeout);
+
+    if (sres == SOCKET_ERROR)
+        return make_error_code(MVLCErrorCode::SocketError);
+
+    ssize_t res = ::recv(sockfd, reinterpret_cast<char *>(dest), size, 0);
+
+    //logger->trace("::recv res={}", res);
+
+    if (res == SOCKET_ERROR)
+    {
+        int err = WSAGetLastError();
+
+        if (err == WSAETIMEDOUT || err == WSAEWOULDBLOCK)
+            return make_error_code(MVLCErrorCode::SocketReadTimeout);
+
+        return make_error_code(MVLCErrorCode::SocketError);
+    }
+
+#if 0
+    if (res >= static_cast<ssize_t>(sizeof(u32)))
+    {
+        util::log_buffer(
+            std::cerr,
+            basic_string_view<const u32>(reinterpret_cast<const u32 *>(dest), res / sizeof(u32)),
+            "32-bit words in buffer from ::recv()");
+    }
+#endif
+
+    bytesTransferred = res;
+
+    return {};
+}
+#else
+std::error_code receive_one_packet(int sockfd, u8 *dest, size_t size,
+                                                 u16 &bytesTransferred, int)
+{
+    bytesTransferred = 0u;
+
+    ssize_t res = ::recv(sockfd, reinterpret_cast<char *>(dest), size, 0);
+
+    if (res < 0)
+    {
+        if (errno == EAGAIN || errno == EWOULDBLOCK)
+            return make_error_code(MVLCErrorCode::SocketReadTimeout);
+
+        return std::error_code(errno, std::system_category());
+    }
+
+    bytesTransferred = res;
+    return {};
+}
+#endif
+
+
 Impl::Impl(const std::string &host)
     : m_host(host)
     , m_throttleCounters()
@@ -1093,73 +1161,6 @@ std::error_code Impl::write(Pipe pipe, const u8 *buffer, size_t size,
 
     return write_to_socket(getSocket(pipe), buffer, size, bytesTransferred);
 }
-
-#ifdef __WIN32
-static inline std::error_code receive_one_packet(int sockfd, u8 *dest, size_t size,
-                                                 u16 &bytesTransferred, int timeout_ms)
-{
-    fd_set fds;
-    FD_ZERO(&fds);
-    FD_SET(sockfd, &fds);
-
-    struct timeval tv = ms_to_timeval(timeout_ms);
-
-    int sres = ::select(0, &fds, nullptr, nullptr, &tv);
-
-    if (sres == 0)
-        return make_error_code(MVLCErrorCode::SocketReadTimeout);
-
-    if (sres == SOCKET_ERROR)
-        return make_error_code(MVLCErrorCode::SocketError);
-
-    ssize_t res = ::recv(sockfd, reinterpret_cast<char *>(dest), size, 0);
-
-    //logger->trace("::recv res={}", res);
-
-    if (res == SOCKET_ERROR)
-    {
-        int err = WSAGetLastError();
-
-        if (err == WSAETIMEDOUT || err == WSAEWOULDBLOCK)
-            return make_error_code(MVLCErrorCode::SocketReadTimeout);
-
-        return make_error_code(MVLCErrorCode::SocketError);
-    }
-
-#if 0
-    if (res >= static_cast<ssize_t>(sizeof(u32)))
-    {
-        util::log_buffer(
-            std::cerr,
-            basic_string_view<const u32>(reinterpret_cast<const u32 *>(dest), res / sizeof(u32)),
-            "32-bit words in buffer from ::recv()");
-    }
-#endif
-
-    bytesTransferred = res;
-
-    return {};
-}
-#else
-static inline std::error_code receive_one_packet(int sockfd, u8 *dest, size_t size,
-                                                 u16 &bytesTransferred, int)
-{
-    bytesTransferred = 0u;
-
-    ssize_t res = ::recv(sockfd, reinterpret_cast<char *>(dest), size, 0);
-
-    if (res < 0)
-    {
-        if (errno == EAGAIN || errno == EWOULDBLOCK)
-            return make_error_code(MVLCErrorCode::SocketReadTimeout);
-
-        return std::error_code(errno, std::system_category());
-    }
-
-    bytesTransferred = res;
-    return {};
-}
-#endif
 
 PacketReadResult Impl::read_packet(Pipe pipe_, u8 *buffer, size_t size)
 {
