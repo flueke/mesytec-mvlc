369f9eaf6bc4cd4d5f2a9281769118b8d72020af
Florian Lüke <f.lueke@mesytec.com>
2024-04-24 10:10:17 +0200

    windows dll export fixes

--- src/mesytec-mvlc/util/udp_sockets.h
+++ src/mesytec-mvlc/util/udp_sockets.h
@@ -20,20 +20,20 @@ namespace mesytec::mvlc::eth
 //
 // Returns the socket on success or -1 on error. If ecp is non-null and an
 // error occurs it will be stored in *ecp.
-int connect_udp_socket(const std::string &remoteHost, u16 remotePort, std::error_code *ecp = nullptr);
+int MESYTEC_MVLC_EXPORT connect_udp_socket(const std::string &remoteHost, u16 remotePort, std::error_code *ecp = nullptr);
 
 // Returns an unconnected UDP socket bound to the specified local port or -1 on
 // error. If ecp is non-null and an error occurs it will be stored in *ecp.
-int bind_udp_socket(u16 localPort, std::error_code *ecp = nullptr);
+int MESYTEC_MVLC_EXPORT bind_udp_socket(u16 localPort, std::error_code *ecp = nullptr);
 
 // Returns the local port the socket is bound to or 0 on error. If ecp is
 // non-null and an error occurs it will be stored in *ecp.
-u16 get_local_socket_port(int sock, std::error_code *ecp = nullptr);
+u16 MESYTEC_MVLC_EXPORT get_local_socket_port(int sock, std::error_code *ecp = nullptr);
 
-std::error_code set_socket_write_timeout(int sock, unsigned ms);
-std::error_code set_socket_read_timeout(int sock, unsigned ms);
+std::error_code MESYTEC_MVLC_EXPORT set_socket_write_timeout(int sock, unsigned ms);
+std::error_code MESYTEC_MVLC_EXPORT set_socket_read_timeout(int sock, unsigned ms);
 
-std::error_code close_socket(int sock);
+std::error_code MESYTEC_MVLC_EXPORT close_socket(int sock);
 
 // Does IPv4 host lookup for a UDP socket. On success the resulting struct
 // sockaddr_in is copied to dest.
