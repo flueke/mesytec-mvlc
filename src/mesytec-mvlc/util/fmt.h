63ea417ff297e188e9d87fd491ddbfc68b0d2684
Florian Lüke <f.lueke@mesytec.com>
2024-10-15 00:24:11 +0200

    fmt: add generic formatter for std::pair
    hope this won't bite somehow

--- src/mesytec-mvlc/util/fmt.h
+++ src/mesytec-mvlc/util/fmt.h
@@ -3,4 +3,12 @@
 
 #include <spdlog/fmt/fmt.h>
 
+template <typename First, typename Second> struct fmt::formatter<std::pair<First, Second>>: formatter<string_view>
+{
+    auto format(const std::pair<First, Second> &p, format_context &ctx) const
+    {
+        return format_to(ctx.out(), "({}, {})", p.first, p.second);
+    }
+};
+
 #endif /* __MESYTEC_MVLC_FMT_H__ */
