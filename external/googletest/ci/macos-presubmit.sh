d602b6af6f744bd84df09c71c45f530226c65fbd
Florian Lüke <f.lueke@mesytec.com>
2025-04-25 11:25:35 +0200

    update googltest to v1.16.0

--- external/googletest/ci/macos-presubmit.sh
+++ external/googletest/ci/macos-presubmit.sh
@@ -31,14 +31,16 @@
 
 set -euox pipefail
 
-SRC=$(realpath git/googletest)
+if [[ -z ${GTEST_ROOT:-} ]]; then
+  GTEST_ROOT="$(realpath $(dirname ${0})/..)"
+fi
 
 # Test the CMake build
 for cmake_off_on in OFF ON; do
   BUILD_DIR=$(mktemp -d build_dir.XXXXXXXX)
   cd ${BUILD_DIR}
-  time cmake ${SRC} \
-    -DCMAKE_CXX_STANDARD=11 \
+  time cmake ${GTEST_ROOT} \
+    -DCMAKE_CXX_STANDARD=14 \
     -Dgtest_build_samples=ON \
     -Dgtest_build_tests=ON \
     -Dgmock_build_tests=ON \
@@ -51,7 +53,7 @@ done
 # Test the Bazel build
 
 # If we are running on Kokoro, check for a versioned Bazel binary.
-KOKORO_GFILE_BAZEL_BIN="bazel-3.7.0-darwin-x86_64"
+KOKORO_GFILE_BAZEL_BIN="bazel-7.0.0-darwin-x86_64"
 if [[ ${KOKORO_GFILE_DIR:-} ]] && [[ -f ${KOKORO_GFILE_DIR}/${KOKORO_GFILE_BAZEL_BIN} ]]; then
   BAZEL_BIN="${KOKORO_GFILE_DIR}/${KOKORO_GFILE_BAZEL_BIN}"
   chmod +x ${BAZEL_BIN}
@@ -59,12 +61,16 @@ else
   BAZEL_BIN="bazel"
 fi
 
-cd ${SRC}
+cd ${GTEST_ROOT}
 for absl in 0 1; do
   ${BAZEL_BIN} test ... \
     --copt="-Wall" \
     --copt="-Werror" \
+    --copt="-Wundef" \
+    --cxxopt="-std=c++14" \
     --define="absl=${absl}" \
+    --enable_bzlmod=true \
+    --features=external_include_paths \
     --keep_going \
     --show_timestamps \
     --test_output=errors
