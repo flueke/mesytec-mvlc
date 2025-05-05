da4bdd7a9628c27709dea8cd88ce1cb3b52994b9
Ron Fox <fox@frib.msu.edu>
2025-05-05 10:29:47 -0400

    Issue #5 Improve command processing.
    Added base class for Tcl command extensions for fribdaq-readout

---
+++ extras/fribdaq/src/ReadoutCommand.cc
@@ -0,0 +1,41 @@
+/**
+ * @file ReadoutCommand.cpp
+ * @brief Implementation of  class for all Tcl command recongized by fribdaq-readout
+ * @author Ron Fox <fox @ frib dot msu dot edu>
+ * 
+ *
+ *   This software is Copyright by the Board of Trustees of Michigan
+ *   State University (c) Copyright 2025.
+ *
+ *   You may use this software under the terms of the GNU public license
+ *   (GPL).  The terms of this license are described at:
+ *
+ *    http://www.gnu.org/licenses/gpl.txt
+ * 
+ * @note - this file is private to fribdaq's readout and won't be installed.
+ */
+#include "ReadoutCommmand.h"
+
+using mesytec::mvlc::MVLCReadout;
+
+/**
+ *  constructor
+ *     @param interp  - references the interpreter the command rusn on.
+ *     @param command - command that triggers execution of this class's operator().
+ *     @param pState  - pointer to the extended run state.
+ *     @param pReadout - Pointer to the readout object.
+ * 
+ * We just initialize the base class and save the data our subclasses need.
+ */
+ReadoutCommand::ReadoutCommand(
+    CTCLInterpreter& interp, const char* command, 
+    FRIBDAQRunState& pState, MVLCReadout& pReadout
+) :
+    CTCLObjectProcessor(interp, command, TCLPLUS::kfTRUE),
+    m_pRunState(pState), m_pReadout(pReadout)
+{}
+
+/**
+ * destructor
+ */
+ReadoutCommand::~ReadoutCommand() {}
