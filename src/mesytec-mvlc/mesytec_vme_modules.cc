1d48cae9097e5e74b1a4d05be40d89940420dc38
Florian Lüke <f.lueke@mesytec.com>
2025-06-04 15:26:11 +0200

    fix one linker issue with mesytec_vme_modules.h

---
+++ src/mesytec-mvlc/mesytec_vme_modules.cc
@@ -0,0 +1,14 @@
+#include "mesytec_vme_modules.h"
+
+namespace mesytec::mvlc::vme_modules
+{
+    const u16 HardwareIds::MADC_32;
+    const u16 HardwareIds::MQDC_32;
+    const u16 HardwareIds::MTDC_32;
+    const u16 HardwareIds::MDPP_16;
+    const u16 HardwareIds::VMMR_8 ;
+    const u16 HardwareIds::VMMR_16;
+    const u16 HardwareIds::MDPP_32;
+    const u16 HardwareIds::MVLC   ;
+    const u16 HardwareIds::MVHV_4 ;
+}
