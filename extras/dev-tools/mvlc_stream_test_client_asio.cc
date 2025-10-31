3a6d69ea9ec98f5024ac163f65a637d27412e55a
Florian Lüke <f.lueke@mesytec.com>
2025-10-31 12:46:34 +0100

    make the asio client auto reconnect

--- extras/dev-tools/mvlc_stream_test_client_asio.cc
+++ extras/dev-tools/mvlc_stream_test_client_asio.cc
@@ -6,7 +6,37 @@
 
 #include "mvlc_stream_test_support.h"
 
-template <typename Sock> int run_client(Sock &socket);
+template <typename Sock, typename ReconnectFun>
+int run_client(Sock &socket, ReconnectFun reconnect);
+
+std::error_code reconnect_ipc(asio::local::stream_protocol::socket &socket,
+                              const std::string &socketPath)
+{
+    if (socket.is_open())
+        socket.close();
+    spdlog::info("Connecting to IPC socket: {}", socketPath);
+    asio::local::stream_protocol::endpoint endpoint(socketPath);
+    std::error_code ec;
+    socket.connect(endpoint, ec);
+    return ec;
+}
+
+std::error_code reconnect_tcp(asio::ip::tcp::socket &socket, const std::string &tcpHost,
+                              const std::string &tcpPort)
+{
+    if (socket.is_open())
+        socket.close();
+    spdlog::info("Connecting to TCP URL: {}:{}", tcpHost, tcpPort);
+    auto &io_context = static_cast<asio::io_context &>(socket.get_executor().context());
+    asio::ip::tcp::resolver resolver(io_context);
+    std::error_code ec;
+    auto endpoints = resolver.resolve(tcpHost, tcpPort, ec);
+    if (ec)
+        return ec;
+    // asio::ip::tcp::socket socket(socket.get_executor().context());
+    asio::connect(socket, endpoints, ec);
+    return ec;
+}
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +48,7 @@ int main(int argc, char *argv[])
         TCP,
         IPC
     } method = Method::TCP;
+
     spdlog::set_level(spdlog::level::info);
     argh::parser parser({"-h", "--help", "--log-level", "--tcp", "--ipc"});
     parser.parse(argc, argv);
@@ -84,21 +115,16 @@ int main(int argc, char *argv[])
         {
         case Method::IPC:
         {
-            spdlog::info("Connecting to IPC socket: {}", socketPath);
-            asio::local::stream_protocol::endpoint endpoint(socketPath);
             asio::local::stream_protocol::socket socket(io_context);
-            socket.connect(endpoint);
-            ret = run_client(socket);
+            ret = run_client(socket,
+                             std::bind(reconnect_ipc, std::ref(socket), std::ref(socketPath)));
         }
         break;
         case Method::TCP:
         {
-            spdlog::info("Connecting to TCP URL: {}:{}", tcpHost, tcpPort);
-            asio::ip::tcp::resolver resolver(io_context);
-            auto endpoints = resolver.resolve(tcpHost, tcpPort);
             asio::ip::tcp::socket socket(io_context);
-            asio::connect(socket, endpoints);
-            ret = run_client(socket);
+            ret = run_client(socket, std::bind(reconnect_tcp, std::ref(socket), std::ref(tcpHost),
+                                               std::ref(tcpPort)));
         }
         break;
         }
@@ -109,61 +135,113 @@ int main(int argc, char *argv[])
     }
 }
 
-template <typename Sock> int run_client(Sock &socket)
+template <typename Sock, typename ReconnectFun> int run_client(Sock &socket, ReconnectFun reconnect)
 {
+    enum class State
+    {
+        Connecting,
+        Connected,
+    } state = State::Connecting;
+
     std::vector<u8> destBuffer;
     ssize_t lastSeqNum = -1;
+    std::error_code ec;
+
     while (true)
     {
-        destBuffer.resize(sizeof(TestBuffer));
-        auto bytesRead = asio::read(socket, asio::buffer(destBuffer.data(), destBuffer.size()));
-
-        if (bytesRead != destBuffer.size())
+        switch (state)
         {
-            spdlog::error("Connection closed while reading header");
-            return 1;
-        }
+        case State::Connecting:
+            if (ec = reconnect())
+            {
+                spdlog::warn("Failed to connect to server: {}", ec.message());
+                std::this_thread::sleep_for(std::chrono::milliseconds(250));
+            }
+            else
+            {
+                state = State::Connected;
+                spdlog::info("Connected to server");
+                lastSeqNum = -1;
+            }
+            break;
 
-        auto testBuffer = reinterpret_cast<const TestBuffer *>(destBuffer.data());
-
-        spdlog::trace(
-            "Received TestBuffer header: magic={:#010x}, sequence_number={}, buffer_size={} "
-            "words ({} bytes)",
-            testBuffer->magic, testBuffer->sequence_number, testBuffer->buffer_size,
-            testBuffer->buffer_size * sizeof(u32));
-
-        if (lastSeqNum != -1 && testBuffer->sequence_number != static_cast<u32>(lastSeqNum + 1))
+        case State::Connected:
         {
-            spdlog::warn("Buffer loss detected: last seq num {}, current seq num {}", lastSeqNum,
-                         testBuffer->sequence_number);
-            return 1;
-        }
-        lastSeqNum = testBuffer->sequence_number;
 
-        size_t bytesToRead = testBuffer->buffer_size * sizeof(u32);
+            destBuffer.resize(sizeof(TestBuffer));
+            auto bytesRead =
+                asio::read(socket, asio::buffer(destBuffer.data(), destBuffer.size()), ec);
 
-        destBuffer.resize(sizeof(TestBuffer) + bytesToRead);
-        // have to update the pointer after resize!
-        testBuffer = reinterpret_cast<const TestBuffer *>(destBuffer.data());
+            if (ec)
+            {
+                if (ec == asio::error::eof)
+                {
+                    spdlog::warn("Connection closed by server");
+                }
+                else if (ec == asio::error::connection_reset || ec == asio::error::broken_pipe)
+                {
+                    spdlog::warn("Connection lost: {}", ec.message());
+                }
+                else
+                {
+                    spdlog::error("Error while reading from server: {}", ec.message());
+                }
+                state = State::Connecting;
+            }
+            else
+            {
+                if (bytesRead != destBuffer.size())
+                {
+                    spdlog::error("Connection closed while reading header");
+                    return 1;
+                }
 
-        bytesRead =
-            asio::read(socket, asio::buffer(destBuffer.data() + sizeof(TestBuffer), bytesToRead));
+                auto testBuffer = reinterpret_cast<const TestBuffer *>(destBuffer.data());
 
-        if (bytesRead != bytesToRead)
-        {
-            spdlog::error("Connection closed while reading data");
-            return 1;
-        }
+                spdlog::trace("Received TestBuffer header: magic={:#010x}, sequence_number={}, "
+                              "buffer_size={} "
+                              "words ({} bytes)",
+                              testBuffer->magic, testBuffer->sequence_number,
+                              testBuffer->buffer_size, testBuffer->buffer_size * sizeof(u32));
 
-        spdlog::trace("Read {} bytes of data for buffer {}", bytesRead, testBuffer->sequence_number);
+                if (lastSeqNum != -1 &&
+                    testBuffer->sequence_number != static_cast<u32>(lastSeqNum + 1))
+                {
+                    spdlog::warn("Buffer loss detected: last seq num {}, current seq num {}",
+                                 lastSeqNum, testBuffer->sequence_number);
+                    return 1;
+                }
+                lastSeqNum = testBuffer->sequence_number;
+
+                size_t bytesToRead = testBuffer->buffer_size * sizeof(u32);
+
+                destBuffer.resize(sizeof(TestBuffer) + bytesToRead);
+                // have to update the pointer after resize!
+                testBuffer = reinterpret_cast<const TestBuffer *>(destBuffer.data());
+
+                bytesRead = asio::read(
+                    socket, asio::buffer(destBuffer.data() + sizeof(TestBuffer), bytesToRead));
+
+                if (bytesRead != bytesToRead)
+                {
+                    spdlog::error("Connection closed while reading data");
+                    return 1;
+                }
+
+                spdlog::trace("Read {} bytes of data for buffer {}", bytesRead,
+                              testBuffer->sequence_number);
 
 #if 1
-        if (!verify_test_data(destBuffer, testBuffer->sequence_number))
-        {
-            spdlog::error("Data verification failed for buffer {}", testBuffer->sequence_number);
-            break;
-        }
+                if (!verify_test_data(destBuffer, testBuffer->sequence_number))
+                {
+                    spdlog::error("Data verification failed for buffer {}",
+                                  testBuffer->sequence_number);
+                    break;
+                }
 #endif
+            }
+        }
+        }
     }
     return 0;
 }
