36343dafa2931f45de1b79b52b7ee7b1bf167eba
Florian Lüke <f.lueke@mesytec.com>
2025-11-05 15:59:22 +0100

    bit of fiddling with the asio client - not sure where to go with this

--- extras/mvlc_stream_client_asio.cc
+++ extras/mvlc_stream_client_asio.cc
@@ -41,6 +41,7 @@ std::error_code reconnect_tcp(asio::ip::tcp::socket &socket, const std::string &
 
 struct ClientContext
 {
+    bool isRawFormat = false;
     std::vector<std::uint8_t> destBuffer;
     size_t destBufferUsed = 0;
     ssize_t lastSeqNum = -1;
@@ -50,6 +51,209 @@ struct ClientContext
     size_t totalReads = 0;
     size_t totalBuffersReceived = 0;
 };
+
+template<typename Sock>
+size_t read_at_least(Sock &socket, std::uint8_t *dest, size_t bytesNeeded, std::error_code &ec)
+{
+    size_t bytesRead = 0;
+    while (bytesRead < bytesNeeded && !ec)
+    {
+        auto asioDest = asio::buffer(dest + bytesRead, bytesNeeded - bytesRead);
+        totalBytesRead += asio::read(socket, asioDest, ec);
+    }
+    return bytesRead;
+}
+
+template <typename Sock, typename ReconnectFun>
+int run_client(Sock &socket, ReconnectFun reconnect, ClientContext &ctx)
+{
+    enum class State
+    {
+        Connecting,
+        Connected,
+    } state = State::Connecting;
+
+    std::error_code ec;
+
+    while (true)
+    {
+        switch (state)
+        {
+        case State::Connecting:
+            if (ec = reconnect())
+            {
+                spdlog::warn("Failed to connect to server: {}", ec.message());
+                std::this_thread::sleep_for(std::chrono::milliseconds(250));
+            }
+            else
+            {
+                state = State::Connected;
+                spdlog::info("Connected to server");
+                clientState.lastSeqNum = -1;
+                clientState.destBufferUsed = 0;
+            }
+            break;
+
+        case State::Connected:
+        {
+            clientState.destBuffer.resize(util::Megabytes(2));
+            size_t bytesRead = 0;
+
+            while (clientState.destBufferUsed < sizeof(TestBuffer) && !ec)
+            {
+                auto dest =
+                    asio::buffer(clientState.destBuffer.data() + clientState.destBufferUsed,
+                                 clientState.destBuffer.size() - clientState.destBufferUsed);
+
+                bytesRead = asio::read(socket, dest, ec);
+
+                if (!ec)
+                {
+                    clientState.destBufferUsed += bytesRead;
+                    clientState.totalBytesReceived += bytesRead;
+                    clientState.bytesReceivedInInterval += bytesRead;
+                    clientState.totalReads += 1;
+                }
+            }
+
+            if (ec)
+            {
+                if (ec == asio::error::eof)
+                {
+                    spdlog::warn("Connection closed by server");
+                }
+                else if (ec == asio::error::connection_reset || ec == asio::error::broken_pipe)
+                {
+                    spdlog::warn("Connection lost: {}", ec.message());
+                }
+                else
+                {
+                    spdlog::warn("Error while reading from server: {}", ec.message());
+                }
+                state = State::Connecting;
+                break;
+            }
+
+            auto testBuffer = reinterpret_cast<const TestBuffer *>(clientState.destBuffer.data());
+
+            spdlog::trace("Received TestBuffer header: magic={:#010x}, sequence_number={}, "
+                          "buffer_size={} "
+                          "words ({} bytes)",
+                          testBuffer->magic, testBuffer->sequence_number, testBuffer->buffer_size,
+                          testBuffer->buffer_size * sizeof(u32));
+
+            if (testBuffer->magic != MAGIC_PATTERN)
+            {
+                spdlog::error("Invalid magic pattern in received buffer: {:#010x}",
+                              testBuffer->magic);
+                state = State::Connecting;
+                break;
+            }
+
+            if (clientState.lastSeqNum != -1 &&
+                testBuffer->sequence_number != static_cast<u32>(clientState.lastSeqNum + 1u))
+            {
+                spdlog::warn("Buffer loss detected: last seq num {}, current seq num {}",
+                             clientState.lastSeqNum, testBuffer->sequence_number);
+            }
+
+            clientState.lastSeqNum = testBuffer->sequence_number;
+
+            size_t totalBytesNeeded = sizeof(TestBuffer) + testBuffer->buffer_size * sizeof(u32);
+            clientState.destBuffer.resize(
+                std::max(totalBytesNeeded, clientState.destBuffer.size()));
+            // have to update the pointer to the header after resizing!
+            testBuffer = reinterpret_cast<const TestBuffer *>(clientState.destBuffer.data());
+
+            while (clientState.destBufferUsed < totalBytesNeeded && !ec)
+            {
+                auto dest =
+                    asio::buffer(clientState.destBuffer.data() + clientState.destBufferUsed,
+                                 clientState.destBuffer.size() - clientState.destBufferUsed);
+
+                bytesRead = asio::read(socket, dest, ec);
+
+                if (!ec)
+                {
+                    clientState.destBufferUsed += bytesRead;
+                    clientState.totalBytesReceived += bytesRead;
+                    clientState.bytesReceivedInInterval += bytesRead;
+                    clientState.totalReads += 1;
+                }
+            }
+
+            if (ec)
+            {
+                if (ec == asio::error::eof)
+                {
+                    spdlog::warn("Connection closed by server");
+                }
+                else if (ec == asio::error::connection_reset || ec == asio::error::broken_pipe)
+                {
+                    spdlog::warn("Connection lost: {}", ec.message());
+                }
+                else
+                {
+                    spdlog::warn("Error while reading from server: {}", ec.message());
+                }
+                state = State::Connecting;
+                break;
+            }
+
+            ++clientState.buffersReceivedInInterval;
+            ++clientState.totalBuffersReceived;
+
+            std::basic_string_view<u8> bufferView(clientState.destBuffer.data(),
+                                                  sizeof(TestBuffer) +
+                                                      testBuffer->buffer_size * sizeof(u32));
+
+#if 1
+            if (!verify_test_data(bufferView, testBuffer->sequence_number))
+            {
+                spdlog::error("Data verification failed for buffer {}",
+                              testBuffer->sequence_number);
+            }
+#endif
+
+            // Move any remaining data to the front of the buffer
+            if (clientState.destBufferUsed > totalBytesNeeded)
+            {
+                size_t remaining = clientState.destBufferUsed - totalBytesNeeded;
+                std::memmove(clientState.destBuffer.data(),
+                             clientState.destBuffer.data() + totalBytesNeeded, remaining);
+                clientState.destBufferUsed = remaining;
+            }
+            else
+            {
+                clientState.destBufferUsed = 0;
+            }
+
+            if (auto interval = clientState.swReport.get_interval();
+                interval >= std::chrono::seconds(1))
+            {
+                spdlog::info(
+                    "Received in the last {} ms: {:.2f} MB ({} buffers), rate={:.2f} MB/s ({:.2f} "
+                    "buffers/s) (total {} MB received), avg {:.2f} reads per buffer",
+                    std::chrono::duration_cast<std::chrono::milliseconds>(interval).count(),
+                    clientState.bytesReceivedInInterval / util::Megabytes(1) * 1.0,
+                    clientState.buffersReceivedInInterval,
+                    clientState.bytesReceivedInInterval / util::Megabytes(1) * 1.0 /
+                        (std::chrono::duration_cast<std::chrono::milliseconds>(interval).count() *
+                         1.0 / 1000.0),
+                    clientState.buffersReceivedInInterval /
+                        (std::chrono::duration_cast<std::chrono::milliseconds>(interval).count() *
+                         1.0 / 1000.0),
+                    clientState.totalBytesReceived / util::Megabytes(1) * 1.0,
+                    clientState.totalReads * 1.0 / clientState.totalBuffersReceived);
+
+                clientState.swReport.interval();
+                clientState.bytesReceivedInInterval = 0;
+                clientState.buffersReceivedInInterval = 0;
+            }
+        }
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -77,19 +281,22 @@ int main(int argc, char *argv[])
     if (parser[{"-h", "--help"}])
     {
         std::cout << "Usage: " << argv[0] << " [--tcp [host:port]|--ipc [socket_path]] [--raw]"
-                  << " [--log-level level][--trace][--debug][--info][--warn]\n";
+                  << " [--log-level level][--trace][--debug][--info][--warn]\n\n";
+
+        std::cout
+            << " --raw: Expect raw data format without { bufferNumber, bufferSize } headers.\n";
         return 0;
     }
 
     std::string tcpHost = "127.0.0.1";
     std::string tcpPort = "42333";
     std::string socketPath = "/tmp/mvme_stream_server.sock";
-    bool useRawFormat = false;
-    enum class Method
+    bool isRawFormat = false;
+    enum class Transport
     {
         TCP,
         IPC
-    } method = Method::TCP;
+    } method = Transport::TCP;
 
     std::string tmp;
 
@@ -99,7 +306,7 @@ int main(int argc, char *argv[])
         {
             socketPath = tmp;
         }
-        method = Method::IPC;
+        method = Transport::IPC;
     }
     else if (parser["--tcp"] || parser("--tcp") >> tmp)
     {
@@ -116,39 +323,37 @@ int main(int argc, char *argv[])
                 tcpHost = tmp;
             }
         }
-        method = Method::TCP;
+        method = Transport::TCP;
     }
 
     if (parser["--raw"])
     {
-        useRawFormat = true;
+        isRawFormat = true;
     }
 
-    ClientContext clientState;
+    ClientContext clientContext;
+    clientContext.isRawFormat = isRawFormat;
     int ret = 0;
 
-    #if 0
     try
     {
         asio::io_context io_context;
 
         switch (method)
         {
-        case Method::IPC:
+        case Transport::IPC:
         {
             asio::local::stream_protocol::socket socket(io_context);
-            ret =
-                run_client(socket, std::bind(reconnect_ipc, std::ref(socket), std::ref(socketPath)),
-                           clientState);
+            auto reconnectFun = std::bind(reconnect_ipc, std::ref(socket), std::ref(socketPath));
+            ret = run_client(socket, reconnectFun, clientContext);
         }
         break;
-        case Method::TCP:
+        case Transport::TCP:
         {
             asio::ip::tcp::socket socket(io_context);
-            ret = run_client(
-                socket,
-                std::bind(reconnect_tcp, std::ref(socket), std::ref(tcpHost), std::ref(tcpPort)),
-                clientState);
+            auto reconnectFun =
+                std::bind(reconnect_tcp, std::ref(socket), std::ref(tcpHost), std::ref(tcpPort));
+            ret = run_client(socket, reconnectFun, clientContext);
         }
         break;
         }
@@ -157,5 +362,4 @@ int main(int argc, char *argv[])
     {
         std::cerr << e.what() << '\n';
     }
-    #endif
 }
