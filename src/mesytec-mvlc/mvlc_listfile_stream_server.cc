6b40bbe9b8e5322d7082993f4db2fa7c5bd0e556
Florian Lüke <f.lueke@mesytec.com>
2026-02-02 17:14:16 +0100

    stream server listfile: warn on send error, do not throw

--- src/mesytec-mvlc/mvlc_listfile_stream_server.cc
+++ src/mesytec-mvlc/mvlc_listfile_stream_server.cc
@@ -1,5 +1,9 @@
 #include "mesytec-mvlc/mvlc_listfile_stream_server.h"
+#include <thread>
+
 #include "mesytec-mvlc/stream_server_asio.h"
+#include "mesytec-mvlc/util/fmt.h"
+#include "mesytec-mvlc/util/logging.h"
 
 namespace mesytec::mvlc::listfile
 {
@@ -17,6 +21,9 @@ StreamServerListfileWriteHandle::StreamServerListfileWriteHandle(const std::stri
         throw std::runtime_error(
             "StreamServerListfileWriteHandle: Failed to start stream server on " + bindAddress);
     }
+    // Hack to give clients time to connect. TODO: implement a real solution
+    // (flueke 2026: taken from the zmq listfile code).
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
 }
 
 StreamServerListfileWriteHandle::~StreamServerListfileWriteHandle() {}
@@ -24,10 +31,20 @@ StreamServerListfileWriteHandle::~StreamServerListfileWriteHandle() {}
 size_t StreamServerListfileWriteHandle::write(const u8 *data, size_t size)
 {
     auto res = d->streamServer_.sendToAllClients(data, size);
-    if (res != 0 || res == std::numeric_limits<size_t>::max())
+
+    if (res != 0)
     {
-        throw std::runtime_error(
-            "StreamServerListfileWriteHandle::write(): failed to send data to all clients");
+        auto logger = get_logger("mvlc_listfile_stream_server");
+
+        if (res == std::numeric_limits<size_t>::max())
+        {
+            logger->error("StreamServerListfileWriteHandle::write(): internal server error "
+                          "while trying to send listfile data");
+        }
+        else
+        {
+            logger->warn("Failed to send listfile data to {} clients", res);
+        }
     }
     return size;
 }
