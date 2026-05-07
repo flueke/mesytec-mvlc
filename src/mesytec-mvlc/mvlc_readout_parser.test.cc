9d611a0b61ce8ca4f8189ff47ce0f880fe12b50a
Florian Lüke <f.lueke@mesytec.com>
2026-05-07 14:39:07 +0200

    readout_parser: add one small testcase
    Need many more of these... ;-(

--- src/mesytec-mvlc/mvlc_readout_parser.test.cc
+++ src/mesytec-mvlc/mvlc_readout_parser.test.cc
@@ -1,6 +1,6 @@
-#include "vme_constants.h"
 #include "mvlc_readout_parser.h"
 #include "util/logging.h"
+#include "vme_constants.h"
 #include <gtest/gtest.h>
 #include <spdlog/spdlog.h>
 #include <vector>
@@ -8,7 +8,10 @@
 using namespace mesytec::mvlc;
 using namespace mesytec::mvlc::readout_parser;
 
-TEST(ReadoutParser, MakeReadoutParser)
+namespace
+{
+
+ReadoutParserState make_parser_0()
 {
     StackCommandBuilder stack1("stack1");
     stack1.beginGroup("module1_0");
@@ -16,9 +19,39 @@ TEST(ReadoutParser, MakeReadoutParser)
     StackCommandBuilder stack2("stack2");
     stack2.beginGroup("module2_0");
     stack2.addVMEBlockRead(0x2, vme_amods::a32UserBlock, 0xffff, true);
-    std::vector<StackCommandBuilder> readoutStacks = { stack1, stack2 };
+    std::vector<StackCommandBuilder> readoutStacks = {stack1, stack2};
 
-    auto parser = make_readout_parser(readoutStacks, nullptr);
+    return make_readout_parser(readoutStacks, nullptr);
+}
+
+struct ModuleDataStorage
+{
+    std::vector<u32> data;
+};
+
+// Returns a Callback compatible vector of ModuleData structures pointing
+// into the moduleTestData storage.
+std::vector<ModuleData> to_module_data_list(const std::vector<ModuleDataStorage> &moduleTestData)
+{
+    std::vector<ModuleData> result(moduleTestData.size());
+
+    for (size_t mi = 0; mi < moduleTestData.size(); ++mi)
+    {
+        result[mi] = {};
+        result[mi].data.data = moduleTestData[mi].data.data();
+        result[mi].data.size = moduleTestData[mi].data.size();
+        result[mi].dynamicSize = moduleTestData[mi].data.size();
+        result[mi].hasDynamic = true;
+    }
+
+    return result;
+}
+
+} // namespace
+
+TEST(ReadoutParser, MakeReadoutParser)
+{
+    auto parser = make_parser_0();
 
     ASSERT_EQ(parser.readoutStructure.size(), 2);
 
