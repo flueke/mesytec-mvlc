c8273464ae8730d0e6cdedae1faa85ac3908c488
Florian Lüke <f.lueke@mesytec.com>
2026-06-25 12:32:01 +0200

    cmd list tools: add --with-upload-commands and --stack-upload-offset; fix all the things

--- extras/dev-tools/mvlc_command_list_tools.cc
+++ extras/dev-tools/mvlc_command_list_tools.cc
@@ -13,6 +13,7 @@ using namespace mesytec;
 int main(int argc, char *argv[])
 {
     argh::parser parser;
+    parser.add_param("--stack-upload-offset");
     parser.parse(argc, argv);
 
     std::ifstream inputFile;
@@ -36,7 +37,14 @@ int main(int argc, char *argv[])
         input = &inputFile;
     }
 
+    std::string tmpStr;
     const bool withStackStartEnd = parser["--with-stack-start-end"];
+    const bool withUploadCommands = parser["--with-upload-commands"];
+    parser("--stack-upload-offset") >> tmpStr;
+
+    auto uploadOffset = mvlc::parse_unsigned<mvlc::u16>(tmpStr).value_or(0);
+
+
 
     std::vector<mvlc::StackCommand> commands;
 
@@ -57,7 +65,8 @@ int main(int argc, char *argv[])
         }
         catch (const std::exception &e)
         {
-            std::cerr << "Error: could not parse command list line: " << e.what() << "\n";
+            if (!line.empty())
+                std::cerr << "Error: could not parse command list line: " << e.what() << "\n";
             continue;
         }
     }
@@ -71,18 +80,82 @@ int main(int argc, char *argv[])
 
     const int Col1Width = 30;
     const int Col2Width = 20;
-    //fmt::println("Parsed {} commands:", commands.size());
+    fmt::println("Parsed {} commands:", commands.size());
 
-    for (const auto &cmd: commands)
+    mvlc::StackCommandBuilder stackBuilder(commands);
+
+    // Note: have to convert each command individually so we know which
+    // words belong to which command. This information is never needed when
+    // just uploading and executing commands.
+
+    if (withUploadCommands)
     {
-        auto buffer = mvlc::make_stack_buffer(std::vector<mvlc::StackCommand>{cmd});
-        if (buffer.empty())
-            continue;
-        fmt::println("{:#010x}\t# {:50}", buffer[0], mvlc::to_string(cmd));
+        using SuperCT = mvlc::super_commands::SuperCommandType;
 
-        for (size_t i=1; i<buffer.size(); ++i)
+        fmt::println("{:#010x}\t# {:50}",
+                static_cast<mvlc::u32>(SuperCT::CmdBufferStart) << mvlc::super_commands::SuperCmdShift,
+                mvlc::to_string(mvlc::SuperCommand({SuperCT::CmdBufferStart})));
+
+        mvlc::u16 destAddress = mvlc::stacks::StackMemoryBegin + uploadOffset;
+
+        for (const auto &cmd: commands)
         {
-            fmt::println("{:#010x}", buffer[i]);
+            auto stackBuffer = mvlc::make_stack_buffer(std::vector<mvlc::StackCommand>{cmd});
+            std::vector<mvlc::SuperCommand> uploadCommands;
+
+            for (const auto &stackWord: stackBuffer)
+            {
+                mvlc::SuperCommand uploadCmd;
+                uploadCmd.type = mvlc::super_commands::SuperCommandType::WriteLocal;
+                uploadCmd.value = stackWord;
+                uploadCmd.address = destAddress;
+                uploadCommands.push_back(uploadCmd);
+                destAddress += mvlc::AddressIncrement;
+            }
+
+            if (uploadCommands.empty())
+            {
+                spdlog::warn("make_stack_upload_commands() returned empty command list for command: {}", mvlc::to_string(cmd));
+                continue;
+            }
+
+            for (const auto &uploadCmd: uploadCommands)
+            {
+                auto buffer = mvlc::make_command_buffer(std::vector<mvlc::SuperCommand>{uploadCmd});
+                if (buffer.size() < 2)
+                {
+                    spdlog::warn("make_command_buffer() returned empty buffer for upload command: {}", mvlc::to_string(uploadCmd));
+                    continue;
+                }
+
+                fmt::println("{:#010x}\t# {:50}", buffer[1], mvlc::to_string(uploadCmd));
+                // output additional words of the command buffer if any
+                for (size_t i=2; i<buffer.size()-1; ++i)
+                {
+                    fmt::println("{:#010x}", buffer[i]);
+                }
+
+            }
+        }
+
+        fmt::println("{:#010x}\t# {:50}",
+                static_cast<mvlc::u32>(SuperCT::CmdBufferEnd) << mvlc::super_commands::SuperCmdShift,
+                mvlc::to_string(mvlc::SuperCommand({SuperCT::CmdBufferEnd})));
+    }
+    else
+    {
+        for (const auto &cmd: commands)
+        {
+            auto buffer = mvlc::make_stack_buffer(std::vector<mvlc::StackCommand>{cmd});
+            if (buffer.empty())
+                continue;
+            fmt::println("{:#010x}\t# {:50}", buffer[0], mvlc::to_string(cmd));
+
+            // output additional words of the command buffer if any
+            for (size_t i=1; i<buffer.size(); ++i)
+            {
+                fmt::println("{:#010x}", buffer[i]);
+            }
         }
     }
 }
